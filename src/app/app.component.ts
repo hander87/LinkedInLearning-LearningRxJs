f1945e77f6102ca1299959c5648660e37fcd59bc
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-09-05 14:47:24 +0200

    init Course App with UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { Observable, interval, of } from 'rxjs';
-// (RxJs v6 import method for "switchMap")
-import { take, map, filter, switchMap } from 'rxjs/operators';
+import { Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,17 +13,6 @@ export class AppComponent implements OnInit, OnDestroy {
   constructor() {}
 
   ngOnInit() {
-
-    const numbers$ = interval(1000);
-    const letters$ = of('a', 'b', 'c', 'd', 'e');
-
-    // switchMap Outputs only newest value, instead of ALL values
-    letters$.pipe(switchMap( x =>
-      numbers$.pipe(
-        take(5),
-        map(i => i + x)
-      ))
-    ).subscribe( x => console.log(x) );
 
   }
 
