643c50732d186bebdf5f27faf992f0a5587f112f
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-09-05 13:23:44 +0200

    init RxJs Course - LinkedInLearning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'app';
+  observable$;
+
+  constructor() {}
+
+  ngOnInit() {
+
+    this.observable$ = Observable.create((observer) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.complete(3);
+    });
+
+    this.observable$.subscribe(
+      value => console.log(value),
+      err => {},
+      () => console.log('This is the end.')
+    );
+
+  }
+
+  ngOnDestroy() {
+    this.observable$.unsubscribe();
+  }
+
 }
