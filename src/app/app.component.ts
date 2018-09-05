f47b1e5ef2b7d0332da980abee2c7e17d96d41d3
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-09-05 15:12:54 +0200

    debounceTime adds a "lag" on requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-// fromEvent takes element from the DOM
-import { fromEvent, Observable} from 'rxjs';
+import { Subject } from 'rxjs';
+// debounceTime adds a "lag" so only a request gets sent after 200 milliseconds
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,22 @@ import { fromEvent, Observable} from 'rxjs';
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'RxJs Course App';
+  searchString;
+  searchSubject$ = new Subject<string>();
 
   constructor() {}
 
   ngOnInit() {
-    // Creates Observable from a CLICK event
-    fromEvent(document, 'click').subscribe(x => console.log(x));
+    this.searchSubject$
+      .pipe(
+        debounceTime(200)
+      )
+      .subscribe( x => console.log( 'Debounced: ', x ) );
+  }
+
+  inputChanged($event) {
+    console.log('Input changed: ' + $event);
+    this.searchSubject$.next($event);
   }
 
   ngOnDestroy() {
