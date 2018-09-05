b038589b128b2c8140970b5f0a97d4dc35b35a72
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-09-05 16:22:33 +0200

    Working API - Used distinctUntilChanged and tap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-import { Subject } from 'rxjs';
-// debounceTime adds a "lag" so only a request gets sent after 200 milliseconds
-import { debounceTime } from 'rxjs/operators';
+import { Subject, Observable } from 'rxjs';
+import {
+  debounceTime,
+  map,
+  switchMap,
+  distinctUntilChanged,
+  tap
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +16,31 @@ import { debounceTime } from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  title = 'RxJs Course App';
+  title = 'RxJs Course App - Reddit Search';
   searchString;
+  results$: Observable<any>;
   searchSubject$ = new Subject<string>();
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.searchSubject$
+    this.results$ = this.searchSubject$
       .pipe(
-        debounceTime(200)
-      )
-      .subscribe( x => console.log( 'Debounced: ', x ) );
+        debounceTime(200),
+        // Skips value if it is the same as previous value
+        distinctUntilChanged(),
+        // Previously "do" - Does NOTHING! Good for console logs.
+        tap(x => console.log('Logs output: ' + x)),
+        switchMap(searchString => this.queryAPI(searchString))
+      );
+  }
+
+  queryAPI(searchString) {
+    console.log('queryAPI: ', searchString);
+    return this.http.get(`https://www.reddit.com/r/aww/search.json?q=${searchString}`)
+    .pipe(
+      map(result => result['data']['children'])
+    );
   }
 
   inputChanged($event) {
